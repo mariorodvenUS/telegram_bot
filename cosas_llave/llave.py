044686335509923b42897233aeb17f811f011d76
YagoChecaAlonso <yagochecaalonso@gmail.com>
2023-11-05 21:49:03 +0100

    toma de fecha y volcado en json añadido

--- cosas_llave/llave.py
+++ cosas_llave/llave.py
@@ -1,50 +1,52 @@
 import json
-
+import datetime
 
 #def nuevo_llavero():
 
+
 #leemos del json
 with open("llave.json") as archivo:
-    dict_datos = json.load(archivo)
+    datos_llave = json.load(archivo)
     archivo.close()
-print(dict_datos)    
-#chequear si la fecha y la hora son posibles
-#chequeo anio
-if dict_datos["fecha"]["anio"]<2023:    
-    print("año feo")
-#cheqeuo mes    
-if dict_datos["fecha"]["mes"]>13:
-   print("mes feo")     
-#cheqeuo dia   
-if dict_datos["fecha"]["mes"] in [1, 3, 5, 7, 8, 10, 12] and dict_datos["fecha"]["dia"]>31:
-    print("dia feo")   
-elif dict_datos["fecha"]["mes"] in [4, 6, 9, 11] and dict_datos["fecha"]["dia"]>30:
-    print("dia feo")
-elif dict_datos["fecha"]["mes"] == 2 and dict_datos["fecha"]["dia"]>29:
-    print("dia feo")        
-#cheqeuo hora
-if dict_datos["tiempo"]["hora"]>24:
-    print("hora fea")
-if dict_datos["tiempo"]["minuto"]>59:
-    print("minuto feo")
-if dict_datos["tiempo"]["segundo"]>59:
-    print("segundo feo")        
+
+print(datos_llave)    
+escritor = "Alvarito"
+
+
+#conseguimos la fecha cuando se escribio el mensaje y le cambiamos el formato para adaptarlo al .json y librarnos de los microsegundos
+fecha_mensaje = datetime.datetime.now()
+fecha_json =datetime.datetime.strftime(fecha_mensaje, "%Y,%m,%d %H,%M,%S")
+print(fecha_json)
+
+#divimos el string del datetime para poder meterlo guay en un diccionario
+# Split the date and time string by space
+date_str, time_str = fecha_json.split(" ")
+
+# Split the date string by comma
+year, month, day = date_str.split(",")
+
+# Split the time string by comma
+hour, minute, second = time_str.split(",")
+
+# Modificamos el diccionario
+datos_llave = {
+    "llavero": escritor,
+    "fecha":{ 
+            "anio": year,
+            "mes": month,
+            "dia": day,
+    },                  #todas estas variables yo creo que en string estan bien
+    "tiempo": {   
+            "hora": hour,
+            "minuto": minute,
+            "segundo": second
+    }
+}
+print(datos_llave)
+
+
 #escribimos en el json    
 with open("llave.json", "w") as archivo:
-    json.dump(dict_datos, archivo, indent=4)
+    json.dump(datos_llave, archivo, indent=4)
     archivo.close()
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
